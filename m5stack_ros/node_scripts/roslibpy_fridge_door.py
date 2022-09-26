a73db6556d1d2bd31daa419ce7caac017fefc38d
Ubuntu <ubuntu@fridge-pi.jsk.imi.i.u-tokyo.ac.jp>
2022-09-26 11:39:03 +0000

    Add roslibpy_fridge_door.py tmp

---
+++ m5stack_ros/node_scripts/roslibpy_fridge_door.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import rospy
+import time
+from roslibpy import Message, Ros, Topic
+from std_msgs.msg import UInt16
+
+
+class RoslibpyFridgeDoor():
+    """
+    """
+
+    def __init__(self):
+        self.robot_ip_dict = {'fetch15':'133.11.216.217', 'pr1040':'133.11.216.211'} # これは全タスクプログラム共通
+        # self.robot_priority_list = ['pr1040', 'fetch15']
+        self.robot_priority_list = ['fetch15']
+        ## Subscribers
+        rospy.Subscriber('tof', UInt16, self.tof_cb)
+        self.tof = None
+        self.tof_threshold = 30
+        self.door_open_threshold = 60 # 60s = 1min
+        self.door_open_flag = False
+        self.door_open_time = None
+
+    def tof_cb(self, msg):
+        self.tof = msg.data
+        # self.update_last_communication()
+        if self.tof is not None and self.tof > self.tof_threshold: # when door is open
+            if self.door_open_flag == False: # when door first opens
+                self.door_open_flag = True
+                self.door_open_time = time.time()
+            elif time.time() >= self.door_open_time + self.door_open_threshold: # When door is left open
+                self.send_rostopic()
+        else: # when door is not open
+            self.door_open_flag = False
+
+    def send_rostopic(self):
+        for robot in self.robot_priority_list:
+            self.ros_client = Ros(self.robot_ip_dict[robot], 9090)
+            print("wait for server")
+            self.publisher = Topic(self.ros_client, '/test_cmd_vel', 'geometry_msgs/Twist')
+            self.ros_client.run()
+            self.publisher.publish(Message({
+                'linear': {
+                    'x': 0.5,
+                    'y': 0,
+                    'z': 0
+                },
+                'angular': {
+                    'x': 0,
+                    'y': 0,
+                    'z': 0.5
+                }
+            }))
+            rospy.loginfo('Send rostopic because the fridge door is left open')
+            break
+
+if __name__ == '__main__':
+    rospy.init_node('roslibpy_fridge_door')
+    rfd = RoslibpyFridgeDoor()
+    import ipdb
+    ipdb.set_trace()
+    rospy.spin()
